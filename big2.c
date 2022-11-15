e0a9b885dd78ff1b938234e2ca2bb9eb03f64cfe
Manasabhat2222 <117636434+Manasabhat2222@users.noreply.github.com>
2022-11-15 15:48:03 +0530

    Update big2.c

--- big2.c
+++ big2.c
@@ -8,15 +8,15 @@ biggest()
     scanf("%d %d", &num1, &num2);
     if(num1 > num2)
     {
-        printf("%d is Largest\n", num1);
+        printf("%d is Largest amongst all\n", num1);
     }
     else if (num2 > num1)
     {
-        printf("%d is Largest\n", num2);
+        printf("%d is Largest amongst all\n", num2);
     }
     else
     {
-        printf("Both are Equal\n");
+        printf("Both are Equal numbers\n");
     }
    // return 0;
 }
